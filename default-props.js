c08de70e8199f20d711fca8898c890bbdc666db4
wuxingping <41353266+wuxingping@users.noreply.github.com>
2019-02-21 14:50:55 +0800

    Add files via upload

---
+++ default-props.js
@@ -0,0 +1,20 @@
+//雪花形状参考
+var snowShapeObj = {
+    1:'❆',
+    2:'❄',
+    3:'❅',
+    4:'✼',
+    5:'✼',
+    6:'❉',
+    7:'❇',
+    8:'❈',
+    9:'❊',
+    10:'✥',
+    11:'✺'
+};
+var defaultProps = {
+    snowNum:242,
+    snowSpeed:3,
+    snowShape: snowShapeObj[1]
+};
+export default defaultProps;
